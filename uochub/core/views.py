053c131a52739fe5849f3f319e541503d558aada
Josh Birch <joshuabirch@protonmail.com>
2026-08-13 16:14:14 +0100

    move icons to applicable folder, implement onto HTML with basic styling

--- uochub/core/views.py
+++ uochub/core/views.py
@@ -6,24 +6,24 @@ def home(request):
 
 
 def profile(request):
-    return render(request, "core/page.html", {"title": "Profile"})
+    return render(request, "core/page.html", {"title": "Profile", "icon": "icons/profile.svg"})
 
 
 def campus_map(request):
-    return render(request, "core/page.html", {"title": "Campus Map"})
+    return render(request, "core/page.html", {"title": "Campus Map", "icon": "icons/campus-map.svg"})
 
 
 def study_spaces(request):
-    return render(request, "core/page.html", {"title": "Study Spaces"})
+    return render(request, "core/page.html", {"title": "Study Spaces", "icon": "icons/study-spaces.svg"})
 
 
 def wellbeing(request):
-    return render(request, "core/page.html", {"title": "Wellbeing"})
+    return render(request, "core/page.html", {"title": "Wellbeing", "icon": "icons/wellbeing-icon.svg"})
 
 
 def directory(request):
-    return render(request, "core/page.html", {"title": "Campus Directory"})
+    return render(request, "core/page.html", {"title": "Campus Directory", "icon": "icons/campus-directory.svg"})
 
 
 def calendar(request):
-    return render(request, "core/page.html", {"title": "Academic Calendar"})
+    return render(request, "core/page.html", {"title": "Academic Calendar", "icon": "icons/academic-calendar.svg"})
