26f810f50821b8e0e5928402f08be21be072570d
Josh Birch <joshuabirch@protonmail.com>
2026-08-14 14:52:02 +0100

    add studyspace model for implementation

--- uochub/core/admin.py
+++ uochub/core/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Building
+from .models import Building, StudySpace
 
 
 @admin.register(Building)
 class BuildingAdmin(admin.ModelAdmin):
     list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(StudySpace)
+class StudySpaceAdmin(admin.ModelAdmin):
+    list_display = ("name", "building", "slug")
     prepopulated_fields = {"slug": ("name",)}
--- uochub/core/models.py
+++ uochub/core/models.py
@@ -8,3 +8,15 @@ class Building(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class StudySpace(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    building = models.ForeignKey(
+        Building, on_delete=models.CASCADE, related_name="study_spaces"
+    )
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
